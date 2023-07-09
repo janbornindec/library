66e06d8bb4f44931608dea79f7aff42baa836925
janbornindec <janellecwx@gmail.com>
2023-07-09 15:13:00 +1000

    Create card for each book and display it

--- script.js
+++ script.js
@@ -15,16 +15,31 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 };
 
+//target container in html
+const container = document.querySelector('.container');
+
 function displayBook(myLibrary) {
 	for (const book of myLibrary) {
-		console.log(book.title);
+		//create a card for each book
+		const card = document.createElement('div');
+		for (let details in book) {
+			card.textContent = book.info();
+		}
+		//add the card inside the container
+		container.appendChild(card);
 	};
 };
 
+// Testing display book func 
+
 const book1 = new Book("Three Little Pigs","Janelle C","103","not read yet");
 const book2 = new Book("Red Riding Hood","Robin K","83","read");
+const book3 = new Book("Silly Beans","Benjamin J","183","read");
+
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+addBookToLibrary(book3);
+
 
 displayBook(myLibrary);
